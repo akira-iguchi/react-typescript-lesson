fcc0a415bfd237509ef6ba882328bb599e437a0c
akira-iguchi <akituri931@icloud.com>
2021-06-08 17:17:14 +0900

    環境構築

--- src/world.ts
+++ src/world.ts
@@ -8,7 +8,7 @@ export default class World {
   }
 
   // 外部から呼び出せるメソッドを定義
-  public sayHello(elem: HTMLElement | null): void {
+  public sayHello(elem: HTMLElement | null) {
     if (elem) {
       elem.innerText = this.message
     }
